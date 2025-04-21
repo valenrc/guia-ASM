ac03979c81b967d555d5788590dec19306c35308
valenrc <vvalenrc@gmail.com>
2025-04-21 16:34:33 -0300

    introduccion y ABI

--- src/2 - ABI/ABI.asm
+++ src/2 - ABI/ABI.asm
@@ -65,7 +65,6 @@ alternate_sum_4_using_c:
   pop RBP ;pila desalineada, RBP restaurado, RSP apuntando a la dirección de retorno
   ret
 
-
 alternate_sum_4_using_c_alternative:
   ;prologo
   push RBP ;pila alineada
@@ -95,24 +94,51 @@ alternate_sum_4_using_c_alternative:
   pop RBP ;pila desalineada, RBP restaurado, RSP apuntando a la dirección de retorno
   ret
 
-
 ; uint32_t alternate_sum_8(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8);
-; registros y pila: x1[?], x2[?], x3[?], x4[?], x5[?], x6[?], x7[?], x8[?]
+; registros y pila: x1[edi], x2[esi], x3[edx], x4[ecx], x5[r8d], x6[r9d], x7[rbp+16], x8[rbp + 24]
 alternate_sum_8:
 	;prologo
+  push rbp
+  mov rbp, rsp
 
-	; COMPLETAR
+	; (x1 - x2 + x3 - x4) + (x5 - x6 + x7 - x8) = alternate_sum_4(x1,x2,x3,x4) + alternate_sum_4(x5,x6,x7,x8)
+  ; EDI, ESI, EDX, ECX son los arg de alt_sum_4 - el resultado se guarda en eax
+  call alternate_sum_4
+  mov r10d, eax ; guardo en r10d el resultado para despues pasarlo a la funcion sumar_c
+
+  ; paso x5,...,x8 como arg de alt_sum_4
+  mov edi, r8d
+  mov esi, r9d
+  mov edx, [rbp+16]
+  mov ecx, [rbp+24]
+  call alternate_sum_4
+  mov esi, eax ; muevo el resultado al segundo paremetro de sumar_c
+
+  mov edi, r10d ; paso al primer arg lo que habia guardado en r10d
+  call sumar_c ; el resultado se va a guardar en eax
 
 	;epilogo
+  pop rbp
 	ret
-
 
 ; SUGERENCIA: investigar uso de instrucciones para convertir enteros a floats y viceversa
-;void product_2_f(uint32_t * destination, uint32_t x1, float f1);
-;registros: destination[?], x1[?], f1[?]
+;void product_2_f(uint32_t* destination, uint32_t x1, float f1);
+;registros: destination[rdi], x1[esi], f1[xmm0]
 product_2_f:
-	ret
+  push rbp
+  mov rbp, rsp
 
+  movd xmm1, esi      ; guardar x1 en xmm1
+  cvtdq2ps xmm1, xmm1 ; convertirlo a float
+  mulps xmm0, xmm1    ; multiplicarlo por f1 (xmm0)
+  cvttps2dq xmm0, xmm0 ; convertir el resultado a entero de 32 bits
+  movd eax, xmm0       ; guardar el resultado en eax
+
+  ; escribirlo a la direccion en rdi
+  mov [rdi], eax
+
+  pop rbp
+	ret
 
 ;extern void product_9_f(double * destination
 ;, uint32_t x1, float f1, uint32_t x2, float f2, uint32_t x3, float f3, uint32_t x4, float f4
--- src/2 - ABI/main.c
+++ src/2 - ABI/main.c
@@ -9,8 +9,10 @@
 
 int main() {
 	/* Acá pueden realizar sus propias pruebas */
-	assert(alternate_sum_4_using_c(8, 2, 5, 1) == 6);
-
-	assert(alternate_sum_4_using_c_alternative(8, 2, 5, 1) == 6);
+	uint32_t res = alternate_sum_8(822, 230, 481, 566, 592, 70, 838, 216); // esperado: 1651 (822, 230, 481, 566) + (592, 70, 838, 216) = 507 + 1114 = 1651
+	printf("%d\n", res);
+	//assert(alternate_sum_4_using_c(8, 2, 5, 1) == 6);
+	//int res = 0;
+	//product_2_f(&res, 489, 465.01);  // res = 227389,89 = 227388
 	return 0;
 }
